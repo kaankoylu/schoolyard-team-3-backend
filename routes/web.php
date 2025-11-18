cf780786e670a7cd8301af961297451d24628fca
kaankoylu <ekankoylu@outlook.com>
2025-11-18 09:47:47 +0100

    backend_initial_config

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//IGNORE THIS FILE LARAVEL IS ONLY RUNNING FOR BACKEND WE DO NOT NEED THE BUILT IN FRONTEND
+
