dcaef34b1ab1a4bc4070959ed74891b0cdfd48d8
kaankoylu <ekankoylu@outlook.com>
2025-11-24 20:55:08 +0100

    login_backend_implementation_nyw

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
 
-//IGNORE THIS FILE LARAVEL IS ONLY RUNNING FOR BACKEND WE DO NOT NEED THE BUILT IN FRONTEND
-
+require __DIR__.'/auth.php';
