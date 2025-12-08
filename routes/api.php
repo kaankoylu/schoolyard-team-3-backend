9311de4ed841d616008bbdc0a0ea5d663d9fd23a
Shiven Bechan <bechanshiven@gmail.com>
2025-12-08 17:15:39 +0100

    Handle session only if present in auth controller
    Updated AuthenticatedSessionController to check for session presence before regenerating or invalidating it, preventing errors on API routes without sessions. Also added API route for user registration in routes/api.php.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
@@ -12,7 +12,7 @@ Route::get('/ping', function () {
 
 // ---- Authentication ----
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-
+Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth:sanctum');
 
@@ -20,6 +20,8 @@ Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
 
 // ---- DESIGN API ----
 //
